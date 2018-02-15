1952ac75946913134b0ec24d29acbc7db64ee89b
srbberlin <info@hans-schrieber.de>
2018-02-15 13:34:40 +0100

    Latest

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
@@ -12,30 +14,34 @@ var In = function (_React$Component) {
   function In(props) {
     _classCallCheck(this, In);
 
-    var _this = _possibleConstructorReturn(this, _React$Component.call(this, props));
+    var _this = _possibleConstructorReturn(this, (In.__proto__ || Object.getPrototypeOf(In)).call(this, props));
 
     _this.onChange = _this.onChange.bind(_this);
     return _this;
   }
 
-  In.prototype.onChange = function onChange(e) {
-    this.props.publish(e.target.value);
-  };
-
-  In.prototype.render = function render() {
-    return React.createElement(
-      'div',
-      { className: 'col-sm-6' },
-      React.createElement(
-        'h3',
-        null,
-        'Input goes here'
-      ),
-      React.createElement('textarea', {
-        onChange: this.onChange
-      })
-    );
-  };
+  _createClass(In, [{
+    key: 'onChange',
+    value: function onChange(e) {
+      this.props.publish(e.target.value);
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      return React.createElement(
+        'div',
+        { className: 'col-sm-6' },
+        React.createElement(
+          'h3',
+          null,
+          'Input goes here'
+        ),
+        React.createElement('textarea', {
+          onChange: this.onChange
+        })
+      );
+    }
+  }]);
 
   return In;
 }(React.Component);
@@ -46,21 +52,24 @@ var Out = function (_React$Component2) {
   function Out() {
     _classCallCheck(this, Out);
 
-    return _possibleConstructorReturn(this, _React$Component2.apply(this, arguments));
+    return _possibleConstructorReturn(this, (Out.__proto__ || Object.getPrototypeOf(Out)).apply(this, arguments));
   }
 
-  Out.prototype.render = function render() {
-    return React.createElement(
-      'div',
-      { className: 'col-sm-6' },
-      React.createElement(
-        'h3',
-        null,
-        'The result is here'
-      ),
-      React.createElement('div', { id: 'OUT', dangerouslySetInnerHTML: { __html: marked(this.props.text) } })
-    );
-  };
+  _createClass(Out, [{
+    key: 'render',
+    value: function render() {
+      return React.createElement(
+        'div',
+        { className: 'col-sm-6' },
+        React.createElement(
+          'h3',
+          null,
+          'The result is here'
+        ),
+        React.createElement('div', { id: 'OUT', dangerouslySetInnerHTML: { __html: marked(this.props.text) } })
+      );
+    }
+  }]);
 
   return Out;
 }(React.Component);
@@ -71,30 +80,34 @@ var Canvas = function (_React$Component3) {
   function Canvas(props) {
     _classCallCheck(this, Canvas);
 
-    var _this3 = _possibleConstructorReturn(this, _React$Component3.call(this, props));
+    var _this3 = _possibleConstructorReturn(this, (Canvas.__proto__ || Object.getPrototypeOf(Canvas)).call(this, props));
 
     _this3.state = { typed: "" };
     _this3.publish = _this3.publish.bind(_this3);
     return _this3;
   }
 
-  Canvas.prototype.publish = function publish(text) {
-    this.setState({ typed: text });
-  };
-
-  Canvas.prototype.render = function render() {
-    return React.createElement(
-      'div',
-      { className: 'row' },
-      React.createElement(
-        'h1',
-        null,
-        'The Markdown Viewer'
-      ),
-      React.createElement(In, { publish: this.publish }),
-      React.createElement(Out, { text: this.state.typed })
-    );
-  };
+  _createClass(Canvas, [{
+    key: 'publish',
+    value: function publish(text) {
+      this.setState({ typed: text });
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      return React.createElement(
+        'div',
+        { className: 'row' },
+        React.createElement(
+          'h1',
+          null,
+          'The Markdown Viewer'
+        ),
+        React.createElement(In, { publish: this.publish }),
+        React.createElement(Out, { text: this.state.typed })
+      );
+    }
+  }]);
 
   return Canvas;
 }(React.Component);
